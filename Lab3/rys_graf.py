cbf404a64b262cb735ffa46ee574ea8d52bfc1bd
AdamKretowicz <adamkretowicz99@gmail.com>
2023-06-24 21:33:10 +0200

    Lab3

---
+++ Lab3/rys_graf.py
@@ -0,0 +1,79 @@
+import numpy as np
+import networkx as nx
+import matplotlib.pyplot as plt
+
+
+def build_graph():
+    print("Podaj typ grafu:")
+    print("1. Skierowany")
+    print("2. Nieskierowany")
+
+    while True:
+        try:
+            graph_type = int(input("Wybierz opcję (1 lub 2): "))
+            if graph_type in [1, 2]:
+                break
+            else:
+                print("Niepoprawny wybór. Wybierz 1 lub 2.")
+        except ValueError:
+            print("Niepoprawny wybór. Wybierz 1 lub 2.")
+
+    num_vertices = int(input("Podaj liczbę wierzchołków: "))
+
+    adjacency_matrix = np.zeros((num_vertices, num_vertices))
+    adjacency_list = [[] for _ in range(num_vertices)]
+
+    num_edges = int(input("Podaj liczbę połączeń: "))
+    print("Dla każdego połączenia, podaj numery wierzchołków, które są połączone.")
+
+    for _ in range(num_edges):
+        while True:
+            try:
+                vertex1 = int(input("Pierwszy wierzchołek: "))
+                vertex2 = int(input("Drugi wierzchołek: "))
+
+                if vertex1 < 0 or vertex1 >= num_vertices or vertex2 < 0 or vertex2 >= num_vertices:
+                    print("Niepoprawny numer wierzchołka. Spróbuj ponownie.")
+                else:
+                    break
+            except ValueError:
+                print("Niepoprawny numer wierzchołka. Spróbuj ponownie.")
+
+        weight = float(input("Podaj wagę połączenia (jeśli graf jest nieważony, wpisz 1): "))
+
+        adjacency_matrix[vertex1][vertex2] = weight
+        adjacency_list[vertex1].append((vertex2, weight))
+
+        if graph_type == 2:  # Nieskierowany graf
+            adjacency_matrix[vertex2][vertex1] = weight
+            adjacency_list[vertex2].append((vertex1, weight))
+
+    print("\nMacierz sąsiedztwa:")
+    print(adjacency_matrix)
+
+    print("\nLista sąsiedztwa:")
+    for i in range(num_vertices):
+        print(f"{i}: {adjacency_list[i]}")
+
+    # Tworzenie grafu za pomocą biblioteki networkx
+    G = nx.DiGraph() if graph_type == 1 else nx.Graph()
+
+    for i in range(num_vertices):
+        G.add_node(i)
+
+    for i in range(num_vertices):
+        for j in range(num_vertices):
+            if adjacency_matrix[i][j] != 0:
+                G.add_edge(i, j, weight=adjacency_matrix[i][j])
+
+    # Rysowanie grafu
+    pos = nx.spring_layout(G)
+    labels = nx.get_edge_attributes(G, 'weight')
+    nx.draw_networkx(G, pos)
+    nx.draw_networkx_edge_labels(G, pos, edge_labels=labels)
+    plt.show()
+
+
+
+
+build_graph()
